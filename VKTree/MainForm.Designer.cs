5389f917150c54b8bbe46ca42518c9b230fb71a2
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-02-08 23:38:02 +0300

    Добавьте файлы проекта.

---
+++ VKTree/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace VKTree
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserIDTextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AppIDTextBox = new System.Windows.Forms.TextBox();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PassTextBox = new System.Windows.Forms.TextBox();
+            this.GetTokenButton = new System.Windows.Forms.Button();
+            this.TokenReceived = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(405, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VK ID:";
+            // 
+            // UserIDTextBox
+            // 
+            this.UserIDTextBox.Location = new System.Drawing.Point(472, 35);
+            this.UserIDTextBox.Name = "UserIDTextBox";
+            this.UserIDTextBox.Size = new System.Drawing.Size(196, 20);
+            this.UserIDTextBox.TabIndex = 1;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Enabled = false;
+            this.StartButton.Location = new System.Drawing.Point(684, 33);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(488, 96);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 238);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(43, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Application ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(43, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(43, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // AppIDTextBox
+            // 
+            this.AppIDTextBox.Location = new System.Drawing.Point(176, 33);
+            this.AppIDTextBox.Name = "AppIDTextBox";
+            this.AppIDTextBox.Size = new System.Drawing.Size(152, 20);
+            this.AppIDTextBox.TabIndex = 7;
+            this.AppIDTextBox.Text = "6852311";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(121, 71);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(152, 20);
+            this.LoginTextBox.TabIndex = 8;
+            // 
+            // PassTextBox
+            // 
+            this.PassTextBox.Location = new System.Drawing.Point(140, 106);
+            this.PassTextBox.Name = "PassTextBox";
+            this.PassTextBox.PasswordChar = '*';
+            this.PassTextBox.Size = new System.Drawing.Size(152, 20);
+            this.PassTextBox.TabIndex = 9;
+            // 
+            // GetTokenButton
+            // 
+            this.GetTokenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GetTokenButton.Location = new System.Drawing.Point(47, 148);
+            this.GetTokenButton.Name = "GetTokenButton";
+            this.GetTokenButton.Size = new System.Drawing.Size(245, 36);
+            this.GetTokenButton.TabIndex = 10;
+            this.GetTokenButton.Text = "Get Access Token";
+            this.GetTokenButton.UseVisualStyleBackColor = true;
+            this.GetTokenButton.Click += new System.EventHandler(this.GetTokenButton_Click);
+            // 
+            // TokenReceived
+            // 
+            this.TokenReceived.AutoSize = true;
+            this.TokenReceived.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TokenReceived.ForeColor = System.Drawing.Color.Firebrick;
+            this.TokenReceived.Location = new System.Drawing.Point(73, 212);
+            this.TokenReceived.Name = "TokenReceived";
+            this.TokenReceived.Size = new System.Drawing.Size(191, 20);
+            this.TokenReceived.TabIndex = 11;
+            this.TokenReceived.Text = "Have no access token!";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 448);
+            this.Controls.Add(this.TokenReceived);
+            this.Controls.Add(this.GetTokenButton);
+            this.Controls.Add(this.PassTextBox);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.AppIDTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.UserIDTextBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VKTree";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserIDTextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AppIDTextBox;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.TextBox PassTextBox;
+        private System.Windows.Forms.Button GetTokenButton;
+        private System.Windows.Forms.Label TokenReceived;
+    }
+}
+
---
+++ VKTree/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace VKTree
+{
+    public partial class MainForm : Form
+    {
+        VkApi vk = new VkApi();
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!Int64.TryParse(UserIDTextBox.Text, out long id))
+            {
+                MessageBox.Show("Cannot resolve field User ID!", "Error");
+                return;
+            }
+            var p = vk.Users.Get(new long[] { id }).FirstOrDefault();
+            listBox1.Items.Add(p.FirstName + " " + p.LastName);
+            var users = vk.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            {
+                UserId = id,
+                Fields = ProfileFields.FirstName,
+            });
+            foreach (User u in users)
+            {
+                listBox1.Items.Add(u.FirstName + " " + u.LastName);
+            }
+        }
+
+        private void GetTokenButton_Click(object sender, EventArgs e)
+        {
+            if (!UInt64.TryParse(AppIDTextBox.Text, out ulong aid))
+            {
+                MessageBox.Show("Cannot resolve field Application ID!", "Error");
+                return;
+            }
+            try
+            {
+                vk.Authorize(new ApiAuthParams
+                {
+                    ApplicationId = aid,
+                    Login = LoginTextBox.Text,
+                    Password = PassTextBox.Text,
+                    Settings = Settings.All
+                });
+            }
+            catch (VkNet.Exception.VkApiException)
+            {
+                TokenReceived.Text = "Have no access token!";
+                TokenReceived.ForeColor = Color.Firebrick;
+                StartButton.Enabled = false;
+                return;
+            }
+            finally
+            {
+                TokenReceived.Text = "Have no access token!";
+                TokenReceived.ForeColor = Color.Firebrick;
+                StartButton.Enabled = false;
+            }
+            TokenReceived.Text = "Access token received!";
+            TokenReceived.ForeColor = Color.Green;
+            StartButton.Enabled = true;
+        }
+    }
+}
