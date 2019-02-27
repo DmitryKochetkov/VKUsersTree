5e6cddf3bcc1c645bf7fd943a203c21a85cd4bb1
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-02-27 20:15:56 +0300

    UserEquality project
    Нужно выяснить, как по-нормальному сравнивать объекты класса User.

---
+++ UserEquality/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+
+namespace UserEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VkApi vk = new VkApi();
+            Console.WriteLine("Password for dimitri.kochetkov@gmail.com: ");
+            string s = Console.ReadLine();
+            try
+            {
+                vk.Authorize(new VkNet.Model.ApiAuthParams
+                {
+                    ApplicationId = 6852311,
+                    Login = "dimitri.kochetkov@gmail.com",
+                    Password = s,
+                    Settings = VkNet.Enums.Filters.Settings.Offline
+                });
+            }
+            finally
+            {
+                Console.WriteLine("Authorization error :(");
+            }
+
+            var Artem = vk.Users.Get(new string[] { "artembedrikov" });
+            //var f = 
+
+            Console.ReadKey();
+        }
+    }
+}
